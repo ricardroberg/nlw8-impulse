7f5753a3b2a828b6915fb41ce947bdd6b759d400
Ricard Roberg <ricardroberg@gmail.com>
2022-05-02 19:58:36 -0300

    End of class 01

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,5 @@
-interface ButtonProps {
-  text?: string;
-}
+import { Widget } from "./components/Widget";
 
-function Button(props: ButtonProps) {
-  return <button>{props.text ?? "Default"}</button>;
+export function App() {
+  return <Widget />;
 }
-
-function App() {
-  return (
-    <div>
-      <Button text="Enviar" />
-      <Button text="Ok" />
-    </div>
-  );
-}
-
-export default App;

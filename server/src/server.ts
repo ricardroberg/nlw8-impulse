8f3007d92a120c6968b1cce6266165b6fd81d75e
ricardroberg <ricardroberg@gmail.com>
2022-05-04 19:10:44 -0300

    POST method working

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,54 @@
+import { prisma } from "./prisma";
 import express from "express";
+import nodemailer from "nodemailer";
 
 const app = express();
 
-app.get("/users", (req, res) => {
-  return res.send("Hello World");
+app.use(express.json());
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "7ebb6c17e26ec2",
+    pass: "48e70da7bef3a1",
+  },
+});
+
+app.post("/feedbacks", async (req, res) => {
+  const { type, comment, screenshot } = req.body;
+
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot,
+    },
+  });
+
+  await transport.sendMail({
+    from: "Equipe Feedget <oi@feedget.com>",
+    to: "Ricard Roberg <ricardroberg@gmail.com>",
+    subject: "Novo feedback",
+    html: [
+      `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+      `<p>Tipo do feedback: ${type}</p>`,
+      `<p>Comentário:  ${comment}</p>`,
+      `</div>`,
+    ].join("\n"),
+  });
+
+  return res.status(201).json({ data: feedback });
 });
 
 app.listen(3333, () => {
   console.log("HTTP Server Running!");
 });
+
+// GET, POST, PUT, PATCH, DELETE
+
+// GET = Buscar informações
+// POST = Cadastrar informações
+// PUT = Atualizar informações de uma entidade
+//PATCH = Atualizar uma informação única de uma entidade
+// DELETE = Deletar uma informação

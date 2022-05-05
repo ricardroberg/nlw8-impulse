48b4abcd5a3dc3c57ca2801be2f83cb7eb47af65
ricardroberg <ricardroberg@gmail.com>
2022-05-04 21:16:14 -0300

    Backend finished

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,45 +1,10 @@
-import { prisma } from "./prisma";
 import express from "express";
-import nodemailer from "nodemailer";
+import { routes } from "./routes";
 
 const app = express();
 
 app.use(express.json());
-
-const transport = nodemailer.createTransport({
-  host: "smtp.mailtrap.io",
-  port: 2525,
-  auth: {
-    user: "7ebb6c17e26ec2",
-    pass: "48e70da7bef3a1",
-  },
-});
-
-app.post("/feedbacks", async (req, res) => {
-  const { type, comment, screenshot } = req.body;
-
-  const feedback = await prisma.feedback.create({
-    data: {
-      type,
-      comment,
-      screenshot,
-    },
-  });
-
-  await transport.sendMail({
-    from: "Equipe Feedget <oi@feedget.com>",
-    to: "Ricard Roberg <ricardroberg@gmail.com>",
-    subject: "Novo feedback",
-    html: [
-      `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
-      `<p>Tipo do feedback: ${type}</p>`,
-      `<p>Comentário:  ${comment}</p>`,
-      `</div>`,
-    ].join("\n"),
-  });
-
-  return res.status(201).json({ data: feedback });
-});
+app.use(routes)
 
 app.listen(3333, () => {
   console.log("HTTP Server Running!");
